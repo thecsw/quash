29ab32033731480e789b6cc5943e46b266bc70c3
Jacob McNamee <jacobmcnamee@yahoo.com>
2021-10-06 17:01:44 -0500

    Added pipe functionality

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"syscall"
@@ -12,34 +13,53 @@ func main() {
 	fmt.Printf("> ")
 	fmt.Scanf("%s", &input)
 
-	// Create pipes
-	// pipeOneRead, pipeOneWrite, _ := os.Pipe()
-	// pipeTwoRead, pipeTwoWrite, _ := os.Pipe()
-
 	commands := strings.Split(input, "|")
-	for _, command := range commands {
+	fmt.Println(commands)
+	pipeRead := make([]*os.File, len(commands)-1)
+	pipeWrite := make([]*os.File, len(commands)-1)
+
+	for index := range pipeRead {
+		pipeRead[index], pipeWrite[index], _ = os.Pipe()
+	}
+
+	for index, command := range commands {
 
 		paths, _ := exec.LookPath(command)
 
-		pid, err := syscall.ForkExec(
+		//pid, err := syscall.ForkExec(
+		_, err := syscall.ForkExec(
 			paths, []string{command}, &syscall.ProcAttr{
 				Dir:   "",
-				Env:   []string{},
-				Files: []uintptr{
-					// os.Stdin.Fd(),
-					// pipeOneWrite.Fd()},
-					// pipeOneRead.Fd(),
-					// pipeTwoWrite.Fd()
-					// ...
-					// pipeNRead().Fd()
-					// os.Stdout.Fd()
-				},
-				Sys: &syscall.SysProcAttr{},
+				Env:   os.Environ(),
+				Files: fileDescriptor(index, pipeRead, pipeWrite),
+				Sys:   &syscall.SysProcAttr{},
 			})
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("New process's ID:", pid)
+		//fmt.Println("New process's ID:", pid)
 
+	}
+}
+
+func fileDescriptor(index int, readPipe []*os.File, writePipe []*os.File) []uintptr {
+	if index == 0 {
+		return []uintptr{
+			os.Stdin.Fd(),
+			writePipe[0].Fd(),
+			os.Stderr.Fd(),
+		}
+	} else if index == len(readPipe) {
+		return []uintptr{
+			readPipe[index-1].Fd(),
+			os.Stdout.Fd(),
+			os.Stderr.Fd(),
+		}
+	} else {
+		return []uintptr{
+			readPipe[index-1].Fd(),
+			writePipe[index].Fd(),
+			os.Stderr.Fd(),
+		}
 	}
 }
